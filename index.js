2d71dc6e4eba2d50f817d952488972cf3962d1f1
Sumit Sharma <sumits2700@gmail.com>
2023-11-03 20:59:13 +0530

    index.js updated

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 3001; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
